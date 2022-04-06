eb41eea61f299cfe0619dd6f4ad9442e3a7c9cbb
Jérémie Gaffarel <jeremie@gaffarel.pro>
2022-04-06 19:24:57 +0200

    Simplified benchmark stiching (got rid of spaghetti conditions) + save

--- setup/integrator/stich_best_benchmarks.py
+++ setup/integrator/stich_best_benchmarks.py
@@ -14,17 +14,14 @@ import glob
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Load and plot benchmark results from each ascent part
-fig = plt.figure(figsize=(14, 5))
-gs = fig.add_gridspec(1, 2)
-ax1 = fig.add_subplot(gs[0, 0])
-ax2 = fig.add_subplot(gs[0, 1])
-stage_1_mass_hist, stage_2_mass_hist = [], []
-times_stiched = []
-states_stiched = []
-dep_vars_stiched = []
+
+times_stiched, states_stiched, dep_vars_stiched = [], [], []
+f_evals_stiched = 0
+
+# Loop trough all stage and phases of the ascent
 for stage in [1, 2]:
     for powered in [True, False]:
+        # Load the corresponding benchmark result file
         f_name = "/data/best_integrator_dt/%i_%s_dt_*.npz" % (stage, "V" if powered else "X")
         try:
             last_state_fname = glob.glob(sys.path[0]+f_name)[0]
@@ -32,39 +29,45 @@ for stage in [1, 2]:
             print("No files found for stage %i, %s" % (stage, "powered" if powered else "unpowered"))
             continue
         saved_results = np.load(last_state_fname)
+
+        # Extract the times and states
         times = saved_results["times"]
         states = saved_results["states"]
         dep_vars = saved_results["dep_vars"]
+        f_evals = saved_results["f_evals"]
         altitudes = dep_vars[:, 0]
 
-        # Plot altitude as a function of time
-        ax1.plot(times, altitudes/1e3, label="Stage %i, %s" % (stage, "powered" if powered else "unpowered"))
+        # Save the final mass if thrust was used
+        if powered:
+            final_mass = states[-1,-1]
+        # Add the constant mass to the states if no thrust was used
+        else:
+            states = np.concatenate([states, np.ones((states.shape[0],1),dtype=states.dtype)*final_mass], axis=1)
+
+        # Save the times and states
         times_stiched.append(times)
         dep_vars_stiched.append(altitudes)
-        if stage == 2 and powered:
-            m_times = [stage_1_mass_hist[0], times[0]]
-            masses = [stage_1_mass_hist[1], stage_1_mass_hist[1]]
-            ax2.plot(m_times, masses, label="Stage 1, unpowered")
-        if powered:
-            masses = states[:, -1]
-            if stage == 1:
-                ax2.plot(times, masses, label="Stage 1, powered")
-            else:
-                ax2.plot([times[0], *times], [stage_1_mass_hist[1], *masses], label="Stage 2, powered")
-        elif stage == 2:
-            ax2.plot([times[0], times[-1]], [stage_2_mass_hist[0], stage_2_mass_hist[0]], label="Stage 2, unpowered")
-        if stage == 1 and not powered:
-            stage_1_mass_hist = [times[0], masses[-1]]
-        elif stage == 2 and powered:
-            stage_2_mass_hist = [masses[-1]]
+        states_stiched.append(states)
+        f_evals_stiched += f_evals
 
+
+# Reshape the arrays
 times_stiched = np.asarray(times_stiched).flatten()
 dep_vars_stiched = np.asarray(dep_vars_stiched).reshape((len(times_stiched), 1))
+states_stiched = np.asarray(states_stiched).reshape((len(times_stiched), -1))
 
-# print(times_stiched, dep_vars_stiched)
+# Save the full benchmark
+np.savez(sys.path[0]+"/data/best_integrator_dt/full_benchmark.npz", times=times_stiched, states=states_stiched, dep_vars=dep_vars_stiched, f_evals=f_evals_stiched)
 
-# input()
-# ax1.plot(times_stiched, dep_vars_stiched[:,0]/1e3, label="Stiched")
+# Plot benchmark results
+fig = plt.figure(figsize=(14, 5))
+gs = fig.add_gridspec(1, 2)
+ax1 = fig.add_subplot(gs[0, 0])
+ax2 = fig.add_subplot(gs[0, 1])
+# Only plot mass up to 13min
+idx_crop = np.where(times_stiched >= 13*60)[0][0]
+ax1.plot(times_stiched/60, dep_vars_stiched[:,0]/1e3, label="Benchmark")
+ax2.plot(times_stiched[:idx_crop]/60, states_stiched[:idx_crop,-1], label="Benchmark")
 
 # Load and plot ascent test results
 f_name = "/data/MAV_ascent_test.npz"
@@ -74,12 +77,14 @@ states = saved_results["states"]
 dep_vars = saved_results["dep_vars"]
 altitudes = dep_vars[:, 1]
 masses = states[:, -1]
-ax1.plot(times, altitudes/1e3, label="Ascent test", linestyle="dotted")
-ax2.plot(times, masses, label="Ascent test", linestyle="dotted")
+idx_crop = np.where(times >= 13*60)[0][0]
+ax1.plot(times/60, altitudes/1e3, label="Variable step test", linestyle="dotted")
+ax2.plot(times[:idx_crop]/60, masses[:idx_crop], label="Variable step test", linestyle="dotted")
 
-ax1.set_xlabel("Time [s]")
+# Prettify plot
+ax1.set_xlabel("Time [min]")
 ax1.set_ylabel("Altitude [km]")
-ax2.set_xlabel("Time [s]")
+ax2.set_xlabel("Time [min]")
 ax2.set_ylabel("Mass [kg]")
 ax1.legend()
 ax2.legend()
