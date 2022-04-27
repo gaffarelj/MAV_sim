029282eafcd9e7586f75ea9ce87dc834272c6490
gaffarelj <jeremie@gaffarel.pro>
2022-04-27 12:10:47 +0200

    Added code to compare variable step integrators

--- setup/integrator/integrator_tuning/compare_integrators.py
+++ setup/integrator/integrator_tuning/compare_integrators.py
@@ -17,6 +17,9 @@ from matplotlib import pyplot as plt
 # Tudatpy imports
 from tudatpy.kernel.numerical_simulation import propagation_setup
 
+# Parameters
+method = "variable" # "variable" or "fixed"
+
 # Connect to the database
 con = sqlite3.connect(sys.path[0]+"/setup/integrator/integrator_tuning/database.db")
 cur = con.cursor()
@@ -34,27 +37,50 @@ fixed_coefficients = [
     propagation_setup.integrator.ralston_4,
     propagation_setup.integrator.three_eight_rule_rk_4
 ]
+variable_coefficients = [
+    propagation_setup.integrator.heun_euler,
+    propagation_setup.integrator.rkf_12,
+    propagation_setup.integrator.rkf_45,
+    propagation_setup.integrator.rkf_56,
+    propagation_setup.integrator.rkf_78,
+    propagation_setup.integrator.rkdp_87,
+    propagation_setup.integrator.rkf_89,
+    propagation_setup.integrator.rkv_89,
+    propagation_setup.integrator.rkf_108,
+    propagation_setup.integrator.rkf_1210,
+    propagation_setup.integrator.rkf_1412
+]
+
+coefficients = variable_coefficients if method == "variable" else fixed_coefficients
 
 fig = plt.figure(figsize=(10, 7))
 
-for integ in fixed_coefficients:
+for integ in coefficients:
     integ_name = str(integ).split(".")[-1]
     print(integ_name)
 
-    dts, f_evals, errors_pos, errors_vel = [], [], [], []
+    f_evals, errors_pos, errors_vel = [], [], []
+    impossible_tol = []
 
     # Get all the data
-    res = cur.execute("SELECT * FROM integrator WHERE method=? AND coefficients=?", ("fixed",integ_name))
+    res = cur.execute("SELECT * FROM integrator WHERE method=? AND coefficients=?", (method, integ_name))
     for row in res:
-        dts.append(row[7])
-        f_evals.append(row[3])
-        errors_pos.append(row[4])
-        errors_vel.append(row[5])
-        print("Dt:", row[7], "Fevals:", row[3], "Error pos:", row[4], "Error vel:", row[5])
+        if method == "variable" and row[4] == -1:
+            if len(impossible_tol) < 2:
+                impossible_tol.append(row[6])
+            else:
+                impossible_tol[1] = row[6]
+        else:
+            f_evals.append(row[3])
+            errors_pos.append(row[4])
+            errors_vel.append(row[5])
+            print("Dt:" if method == "fixed" else "Tol:", row[7] if method == "fixed" else row[6], "Fevals:", row[3], "Error pos:", row[4], "Error vel:", row[5])
+    if method == "variable" and len(impossible_tol) != 0:
+        print("This tolerance range was unfeasible: [%.2e, %.2e]" % (min(impossible_tol), max(impossible_tol)))
 
     # Sort the lists
     try:
-        dts, f_evals, errors_pos, errors_vel = [list(tup) for tup in zip(*sorted(zip(dts, f_evals, errors_pos, errors_vel)))]
+        f_evals, errors_pos, errors_vel = [list(tup) for tup in zip(*sorted(zip(f_evals, errors_pos, errors_vel)))]
     except ValueError:
         print("No data for this integrator")
         continue
@@ -70,6 +96,7 @@ plt.yscale("log")
 plt.xlabel("Number of function evaluations [-]")
 plt.ylabel("Final error in position [m]")
 plt.grid()
+plt.title("Final error in position for different %s step integrators" % method)
 plt.legend()
 plt.tight_layout()
 plt.show()
