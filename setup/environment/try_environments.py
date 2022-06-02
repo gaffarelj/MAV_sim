b4899c4948e67d2b21761ec371629abef765d9d6
gaffarelj <jeremie@gaffarel.pro>
2022-06-02 10:19:51 +0200

    Added analyses of other planets as PM, rad press, and relativis corect

--- setup/environment/try_environments.py
+++ setup/environment/try_environments.py
@@ -25,12 +25,24 @@ from thrust.models.spherical import spherical_SRM
 from thrust.solid_thrust_multi_stage import SRM_thrust_rk4 as SRM_thrust
 
 # Parameters
-analyse_gravity = True
+analyse_gravity = False
+analyse_atmo = True ######### TODO
+analyse_misc = False
+analyse_other_pm = False
 
 n_accs = 0
 if analyse_gravity:
     n_accs = 7
     f_name = "gravity"
+elif analyse_atmo:
+    n_accs = None ######### TODO
+    f_name = "atmo"
+elif analyse_misc:
+    n_accs = 3
+    f_name = "misc"
+elif analyse_other_pm:
+    n_accs = 6
+    f_name = "other_pm"
 allowable_errors = [5e3-3.6e3, 5-3.3]
 
 def environment_names_and_settings(i, acc_dict):
@@ -57,6 +69,32 @@ def environment_names_and_settings(i, acc_dict):
         elif i == 6:
             acc_dict["Mars"].append(propagation_setup.acceleration.spherical_harmonic_gravity(10, 10))
             name = "SH D/O 10"
+    elif analyse_atmo:
+        raise NotImplementedError
+    elif analyse_misc:
+        acc_dict["Mars"] = [
+            propagation_setup.acceleration.aerodynamic(),
+            propagation_setup.acceleration.spherical_harmonic_gravity(4, 4)
+        ]
+        if i == 0:
+            name = "None"
+        elif i == 1:
+            acc_dict["Mars"].append(propagation_setup.acceleration.relativistic_correction(use_schwarzschild = True))
+            name = "Schwarzschild relativistic correction"
+        elif i == 2:
+            acc_dict["Sun"] = [propagation_setup.acceleration.cannonball_radiation_pressure()]
+            name = "Radiation pressure"
+    elif analyse_other_pm:
+        bodies = ["Mars", "Sun", "Jupiter", "Venus", "Earth", "Saturn"]
+        acc_dict["Mars"] = [
+            propagation_setup.acceleration.aerodynamic(),
+            propagation_setup.acceleration.spherical_harmonic_gravity(4, 4)
+        ]
+        if i == 0:
+            name = "None"
+        else:
+            acc_dict[bodies[i]] = [propagation_setup.acceleration.point_mass_gravity()]
+            name = "%s PM" % bodies[i]
     return name, acc_dict
 
 fig1 = plt.figure(figsize=(9,5))
@@ -64,8 +102,6 @@ fig2 = plt.figure(figsize=(9,5))
 base_states = None
 base_label = None
 for i_acc in range(n_accs):
-    print("Running with environment accelerations %i/%i..." % (i_acc+1, n_accs))
-
     # Define problem
     SRM_stage_1 = multi_fin_SRM(R_o=0.24, R_i=0.175, N_f=20, w_f=0.02, L_f=0.05, L=1.05)
     SRM_thrust_model_1 = SRM_thrust(SRM_stage_1, A_t=0.065, epsilon=45)
@@ -105,7 +141,7 @@ for i_acc in range(n_accs):
     # Setup and run simulation for both stages
     stage_res = []
     for stage in [1, 2]:
-        MAV_ascent.create_bodies(stage=stage)
+        MAV_ascent.create_bodies(stage=stage, include_radiation_pressure=analyse_misc, more_bodies=analyse_other_pm)
         accelerations_dict = MAV_ascent.create_accelerations(use_cpp=(stage==1), only_thrust_dict=True, thrust_fname=glob.glob(sys.path[0]+"/data/best_integrator_dt/thrust_%i_dt_*.npz"%stage)[0])
         label_acc, accelerations_dict = environment_names_and_settings(i_acc, accelerations_dict)
         MAV_ascent.acceleration_models = propagation_setup.create_acceleration_models(
@@ -114,6 +150,8 @@ for i_acc in range(n_accs):
             MAV_ascent.bodies_to_propagate,
             MAV_ascent.central_bodies
         )
+        if stage == 1:
+            print("Running with environment accelerations %i/%i: %s..." % (i_acc+1, n_accs, label_acc))
         guidance_object = ascent_framework.FakeAeroGuidance()
         environment_setup.set_aerodynamic_guidance(guidance_object, MAV_ascent.current_body, silence_warnings=True)
         MAV_ascent.create_initial_state()
@@ -166,7 +204,7 @@ for i_acc in range(n_accs):
         diff_times = states_diff_array[:,0]
         positions_diff = np.linalg.norm(states_diff_array[:,1:4], axis=1)
         velocity_diff = np.linalg.norm(states_diff_array[:,4:7], axis=1)
-        label = "%s (vs %s)" % (label_acc, base_label)
+        label = label_acc if base_label == "None" else "%s (vs %s)" % (label_acc, base_label)
         plt.figure(fig1)
         plt.plot(diff_times/60, positions_diff, label=label)
         plt.figure(fig2)
