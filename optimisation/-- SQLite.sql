99cb5c460b127dc0e284e424ec346236ba55e205
gaffarelj <jeremie@gaffarel.pro>
2022-08-08 22:28:44 +0200

    Added more SQL commands frequently used

--- optimisation/-- SQLite.sql
+++ optimisation/-- SQLite.sql
@@ -86,3 +86,56 @@
 -- SELECT id, h_a, h_p FROM solutions_multi_fin WHERE dv_used = "extra_accurate";
 
 -- DELETE FROM solutions_multi_fin WHERE dv_used = "extra_accurate";
+
+-- SELECT h_a, h_p FROM solutions_multi_fin WHERE angle_1 IS NOT NULL AND h_a_score + h_p_score = 0 ORDER BY mass ASC LIMIT 20;
+
+-- SELECT COUNT(*) FROM solutions_multi_fin WHERE h_a > 200e3 AND h_p > 200e3 AND h_a < 500e3 AND h_p < 500e3 AND dv_used = "TVC_only";
+
+-- SELECT COUNT(*) FROM solutions_multi_fin WHERE dv_used = "NSGA2_42_tuning_14";
+
+-- SELECT * FROM solutions_multi_fin WHERE h_p_score + h_a_score = 0 ORDER BY mass_score ASC LIMIT 1;
+-- SELECT * FROM solutions_multi_fin WHERE h_p_score + h_a_score = 0 ORDER BY mass_score DESC LIMIT 1;
+
+-- SELECT * FROM solutions_multi_fin WHERE h_a IS NOT NULL AND h_a_score + h_p_score = 0 ORDER BY t_b_1 + t_b_2 ASC LIMIT 1;
+-- SELECT * FROM solutions_multi_fin WHERE h_a IS NOT NULL AND h_a_score + h_p_score = 0 ORDER BY t_b_1 ASC LIMIT 1;
+-- SELECT * FROM solutions_multi_fin WHERE h_a IS NOT NULL AND h_a_score + h_p_score = 0 ORDER BY t_b_2 ASC LIMIT 1;
+
+-- SELECT COUNT(*) FROM solutions_anchor WHERE angle_1 IS NOT NULL AND h_a_score IS NULL;
+-- SELECT COUNT(*) FROM solutions_anchor WHERE angle_1 IS NOT NULL AND h_a_score IS NOT NULL;
+-- SELECT DISTINCT(dv_used) FROM solutions_anchor;
+-- SELECT id, h_a_score, h_p_score, h_a, h_p, mass, dv_used FROM solutions_anchor WHERE h_a_score IS NOT NULL ORDER BY h_a_score + h_p_score ASC LIMIT 5;
+
+-- SELECT "multi_fin", COUNT(*) FROM solutions_multi_fin;
+
+-- SELECT "tubular", COUNT(*) FROM solutions_tubular;
+-- SELECT DISTINCT(dv_used) FROM solutions_tubular ORDER BY id DESC LIMIT 1;
+-- SELECT id, h_a_score, h_p_score, mass, dv_used, h_a, h_p FROM solutions_tubular WHERE h_a_score IS NOT NULL ORDER BY h_a_score + h_p_score + mass_score ASC LIMIT 5;
+
+-- SELECT "rod_and_tube", COUNT(*) FROM solutions_rod_and_tube;
+
+-- SELECT "anchor", COUNT(*) FROM solutions_anchor;
+
+-- DELETE FROM solutions_multi_fin WHERE dv_used LIKE "extra%";
+-- DELETE FROM solutions_tubular WHERE dv_used LIKE "extra%";
+-- DELETE FROM solutions_rod_and_tube WHERE dv_used LIKE "extra%";
+-- DELETE FROM solutions_anchor WHERE dv_used LIKE "extra%";
+
+SELECT COUNT(*) from solutions_multi_fin WHERE dv_used LIKE "extra%" AND h_a_score IS NOT NULL;
+SELECT COUNT(*) from solutions_tubular WHERE dv_used LIKE "extra%" AND h_a_score IS NOT NULL;
+SELECT COUNT(*) from solutions_rod_and_tube WHERE dv_used LIKE "extra%" AND h_a_score IS NOT NULL;
+SELECT COUNT(*) from solutions_anchor WHERE dv_used LIKE "extra%" AND h_a_score IS NOT NULL;
+
+-- SELECT * FROM solutions_multi_fin WHERE h_a_score = 0 AND h_p_score = 0 ORDER BY mass_score ASC LIMIT 1;
+-- SELECT * FROM solutions_tubular WHERE h_a_score = 0 AND h_p_score = 0 ORDER BY mass_score ASC LIMIT 1;
+-- SELECT * FROM solutions_rod_and_tube WHERE h_a_score = 0 AND h_p_score = 0 ORDER BY mass_score ASC LIMIT 1;
+-- SELECT * FROM solutions_anchor WHERE h_a_score = 0 AND h_p_score = 0 ORDER BY mass_score ASC LIMIT 1;
+-- SELECT * FROM solutions_anchor WHERE h_a_score = 0 AND h_p > 305e3 ORDER BY mass_score ASC LIMIT 1;
+
+-- SELECT * FROM solutions_anchor WHERE h_a_score + h_p_score < 0.7 AND mass_score < 1.0 AND h_a_score IS NOT NULL and angle_1 IS NOT NULL AND dv_used LIKE '%_tuning_%'
+
+-- SELECT * FROM solutions_multi_fin WHERE h_a_score = 0 AND h_p > 305e3 AND mass_score <= 1 ORDER by t_b_2 ASC LIMIT 1;
+-- SELECT t_b_2 FROM solutions_tubular WHERE h_a_score = 0 AND h_p > 305e3 AND mass_score <= 1 ORDER by t_b_2 ASC LIMIT 1;
+-- SELECT t_b_2 FROM solutions_rod_and_tube WHERE h_a_score = 0 AND h_p > 305e3 AND mass_score <= 1 ORDER by t_b_2 ASC LIMIT 1;
+-- SELECT t_b_2 FROM solutions_anchor WHERE h_a_score = 0 AND h_p > 305e3 AND mass_score <= 1 ORDER by t_b_2 ASC LIMIT 1;
+
+-- SELECT * FROM solutions_rod_and_tube WHERE h_a_score + h_p_score = 0 ORDER BY mass_score ASC LIMIT 1;
